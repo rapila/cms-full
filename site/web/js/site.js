014c24e7150157476c8db150985fe459b89ef05a
Raphael Schweikert <any@sabberworm.com>
2011-07-13 13:20:58 +0200

    added example external js library

--- site/web/js/site.js
+++ site/web/js/site.js
@@ -1,7 +1,15 @@
 jQuery(document).ready(function(){
-	jQuery("a[rel^='lightbox']").prettyPhoto({
-	    showTitle: true,
-	    theme: 'dark_square',
-	    allowresize: true
-	  });
+	var color = jQuery.map(jQuery('header').css('background-color').split(','), function(color, i) {
+		return color.match(/\d+/);
+	}).slice(0, 3);
+	color.push(40);
+	jQuery("a[rel^='lightbox']").each(function() {
+		Caman(jQuery(this).find('img')[0], function() {
+			this.colorize.apply(this, color).render();
+		});
+	}).prettyPhoto({
+		showTitle: true,
+		theme: 'dark_square',
+		allowresize: true
+	});
 });

cf3033d207095a810289b886915e681328fa777d
Raphael Schweikert <any@sabberworm.com>
2012-10-23 11:36:48 +0200

    Update prettyPhoto to fancybox for 1.0.0 release of rapila demo site

--- site/web/js/site.js
+++ site/web/js/site.js
@@ -1,27 +1,15 @@
 jQuery(document).ready(function(){
-	var color = jQuery.map(jQuery('header').css('background-color').split(','), function(color, i) {
-		return color.match(/\d+/);
-	}).slice(0, 3);
-	color.push(40);
-	jQuery("a[rel^='lightbox']").each(function() {
-		var link = jQuery(this);
-		Caman(link.find('img')[0], function() {
-			this.colorize.apply(this, color).render();
-		});
-		var license_url = link.attr('data-license-url');
-		var license_image = link.attr('data-license-image');
-		if(license_url && license_image) {
-			license_image = '<a href="'+license_url+'"><img src="'+license_image+'" /></a>';
+	jQuery("a[rel='lightbox']").fancybox({
+		type: 'image',
+		beforeLoad: function() {
+			var link = this.element;
+			var license_url = link.attr('data-license-url');
+			var license_image = link.attr('data-license-image');
+			if(license_url && license_image) {
+				license_image = '<a href="'+license_url+'"><img src="'+license_image+'" /></a>';
+				link.data('license-image-object', license_image);
+			}
+			this.title = link.attr('title') + ' ' + license_image;
 		}
-		link.prettyPhoto({
-			showTitle: true,
-			theme: 'dark_square',
-			allowresize: true,
-			overlay_gallery: false,
-			keyboard_shortcuts: false,
-			gallery_markup: '',
-			social_tools: license_image,
-			deeplinking: false
-		});
 	});
 });
